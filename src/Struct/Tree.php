11008c9a6bb1e514eaceddd438a19a3b15acbf5f
hyb804141892 <804141892@qq.com>
2020-01-23 11:37:10 +0800

    modify struct/tree

--- src/Struct/Tree.php
+++ src/Struct/Tree.php
@@ -9,19 +9,17 @@ trait Tree
 
     public function tree($parentKey = 'parent_id',$childrenKey = 'children'){
 
-        $groups = $this->all()->toArray();
-        $groups = array_column($groups, null, 'id');
-
+        $groups = array_column($this->all()->toArray(), null, 'id');
+		$t = [];
         foreach ($groups as $k => &$value){
             if($value[$parentKey]){
                 $groups[$value[$parentKey]][$childrenKey][] =&$value;
-            }
+            $t[] = $value['id'];
+			}
         }
-        foreach ($groups as $k => $value){
-            if($value[$parentKey]){
-                unset($groups[$k]);
+        foreach ($t as $k => $v){
+                unset($groups[$v]);
             }
-        }
         return $groups;
     }
 
