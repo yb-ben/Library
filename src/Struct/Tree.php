00a7bbe8dcac92cceb367298f4d9b6bb1e7b6139
hyb804141892 <804141892@qq.com>
2020-01-23 11:49:11 +0800

    modify struct/tree

--- src/Struct/Tree.php
+++ src/Struct/Tree.php
@@ -6,15 +6,23 @@ namespace Struct;
 
 trait Tree
 {
+	
+	protected $treeParentKey = 'parent_id';
+	protected $treeChildrenKey = 'nodes';
+	protected $treePrimaryKey = 'id';
 
-    public function tree($parentKey = 'parent_id',$childrenKey = 'children'){
+    public function tree($parentKey = null,$childrenKey = null,$primaryKey = null){
 
-        $groups = array_column($this->all()->toArray(), null, 'id');
+		$parentKey = $parentKey?:$this->treeParentKey;
+		$childrenKey = $childrenKey?:$this->treeChildrenKey;
+		$primaryKey = $primaryKey?:$this->treePrimaryKey;
+	
+        $groups = array_column($this->all()->toArray(), null, $primaryKey);
 		$t = [];
         foreach ($groups as $k => &$value){
             if($value[$parentKey]){
                 $groups[$value[$parentKey]][$childrenKey][] =&$value;
-            $t[] = $value['id'];
+				$t[] = $value[$primaryKey];
 			}
         }
         foreach ($t as $k => $v){
