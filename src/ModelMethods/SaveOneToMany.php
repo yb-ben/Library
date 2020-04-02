33a999175ffef383bcc2fcbc137282e31d7e3b77
804141892@qq.com <804141892@qq.com>
2020-04-02 14:50:33 +0800

    save

---
+++ src/ModelMethods/SaveOneToMany.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace Huyibin\ModelMethods;
+
+trait SaveOneToMany{
+
+    protected  function saveOneToMany(Array $data, $model,String $ref,\Closure $beforeInsert ,\Closure $updater = null,$pk = 'id'){
+
+        if($updater){
+            $model->load($ref);
+            foreach($model->$ref as $r){
+                $flag = 0;
+                foreach($data as $k => $v){
+                    if(isset($v[$pk]) && $r->$pk === $v[$pk]){
+                        $updater && call_user_func_array($updater,[$r,$v,$model]);
+                        unset($data[$k]);
+                        $flag = 1;
+                        break;
+                    }
+                }
+                if($r->isDirty()){
+                    $r->save();
+                }else if(!$flag){
+                    $r->delete();
+                }
+            }
+        }
+        if(!empty($data)){
+            $insert = call_user_func_array($beforeInsert,[$data,$model]);
+            $model->$ref()->saveAll($insert);
+        }
+    }
+}
