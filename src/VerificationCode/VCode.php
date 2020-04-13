e670c7e109961ada2c0143c549dadd3d29622134
huyibin <804141892@qq.com>
2020-04-13 17:00:06 +0800

    save

--- src/VerificationCode/VCode.php
+++ src/VerificationCode/VCode.php
@@ -6,21 +6,38 @@ namespace Huyibin\VerificationCode;
 use Illuminate\Support\Facades\Redis;
 
 class VCode{
+    /**
+     * 允许的字符集
+     * @var string
+     */
+    protected  $str;
 
-    protected  $str = '0123456789';
+    /**
+     * 键前缀
+     * @var string
+     */
+    protected $key ;
 
-    protected $key = 'vcode';
+    /**
+     * 过期时间
+     * @var integer
+     */
+    protected $expire;
 
-    protected $expire = 300;
-
-    public function __construct($str = null)
+    public function __construct($str = null,$key = null)
     {
-
-        $str && ($this->str = $str);
+        $this->str = $str?:'0123456789';
+        $this->key = empty($key)?env('VCODE_KEY','vcode'):$key;
+        $this->expire = env('VCODE_EXPIRE',300);
     }
 
 
-
+    /**
+     * 生成验证码
+     * @param int $size
+     * @return string
+     * @throws \Exception
+     */
     public function generate($size = 6){
 
         $len = mb_strlen($this->str)-1;
@@ -32,8 +49,15 @@ class VCode{
         return $s;
     }
 
+    /**
+     * 生成验证码并存储
+     * @param $identifier
+     * @param int $size
+     * @return string
+     * @throws \Exception
+     */
     public function generateAndStore($identifier,$size = 6){
-          
+
         for($j = 0;$j < 3; $j++){
             $s = $this->generate($size);
             if($this->store($identifier,$s)){
@@ -43,10 +67,15 @@ class VCode{
         throw new \Exception('failed to generate vcode!');
     }
 
-
+    /**
+     * 存储
+     * @param $identifier
+     * @param $code
+     * @return bool
+     */
     public  function store($identifier,$code){
 
-        $key = $this->key.':'.$identifier;
+        $key = $this->getKey($identifier);
         Redis::multi()
         ->set($key,$code)
         ->expire($key,$this->expire)
@@ -55,14 +84,34 @@ class VCode{
         return true;
     }
 
+    /**
+     * 验证
+     * @param $identifier
+     * @param $code
+     * @return bool
+     */
     public function check($identifier,$code){
-        $key = $this->key.':'.$identifier;
+        $key = $this->getKey($identifier);
         $c = Redis::get($key);
-        if($c === $code){
-            Redis::del($key);
-            return true;
-        }
-        return false;
+        return $c === $code;
+    }
+
+    /**
+     * 删除验证码
+     * @param $identifier
+     * @return mixed
+     */
+    public function del($identifier){
+        return Redis::del($this->getKey($identifier));
+    }
+
+    /**
+     * 获取键名
+     * @param $identifier
+     * @return string
+     */
+    protected function getKey($identifier){
+        return  $this->key.':'.$identifier;
     }
 
 }
